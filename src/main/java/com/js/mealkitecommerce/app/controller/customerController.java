56a8ad2db8d8cb24dd21f91fd6dc0c059daf007c
Jae-Soon Kwon <91954574+Jae-soon@users.noreply.github.com>
2022-10-23 01:19:41 +0900

    Feat : Find Password By Username And Email, Refactoring for Exception

--- src/main/java/com/js/mealkitecommerce/app/controller/customerController.java
+++ src/main/java/com/js/mealkitecommerce/app/controller/customerController.java
@@ -1,17 +1,17 @@
 package com.js.mealkitecommerce.app.controller;
 
-import com.js.mealkitecommerce.app.dto.Customer.FindUsernameForm;
-import com.js.mealkitecommerce.app.dto.Customer.JoinForm;
-import com.js.mealkitecommerce.app.dto.Customer.ModifyForm;
-import com.js.mealkitecommerce.app.dto.Customer.ModifyPasswordForm;
+import com.js.mealkitecommerce.app.dto.Customer.*;
 import com.js.mealkitecommerce.app.dto.context.CustomerContext;
 import com.js.mealkitecommerce.app.entity.Customer;
 import com.js.mealkitecommerce.app.exception.DataNotFoundException;
 import com.js.mealkitecommerce.app.exception.EmailDuplicatedException;
+import com.js.mealkitecommerce.app.exception.NotMatchPresentPassword;
 import com.js.mealkitecommerce.app.exception.UserIdDuplicatedException;
+import com.js.mealkitecommerce.app.global.email.service.EmailService;
 import com.js.mealkitecommerce.app.global.util.Util;
 import com.js.mealkitecommerce.app.service.CustomerService;
 import lombok.RequiredArgsConstructor;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -33,6 +33,8 @@ import javax.validation.Valid;
 public class customerController {
     private final CustomerService customerService;
     private final PasswordEncoder passwordEncoder;
+
+    private final EmailService emailService;
 
     @PreAuthorize("isAuthenticated()")
     @GetMapping("/profile")
@@ -64,7 +66,7 @@ public class customerController {
         }
 
         if (customerService.findByEmail(joinForm.getEmail()).isPresent()) {
-            throw new UserIdDuplicatedException("중복된 이메일이 존재합니다.");
+            throw new EmailDuplicatedException("중복된 이메일이 존재합니다.");
         }
 
         if (!joinForm.getPassword().equals(joinForm.getPasswordConfirm())) {
@@ -76,10 +78,10 @@ public class customerController {
         try {
             customerService.join(joinForm);
         } catch (EmailDuplicatedException e) {
-            bindingResult.reject("SignUpEmailDuplicated", e.getMessage());
+            bindingResult.reject("EmailDuplicatedException", e.getMessage());
             return "customer/join";
         } catch(UserIdDuplicatedException e) {
-            bindingResult.reject("SignUpUserIdDuplicated", e.getMessage());
+            bindingResult.reject("UserIdDuplicatedException", e.getMessage());
         } catch (Exception e) {
             e.printStackTrace();
             bindingResult.reject("SignUpFailed", e.getMessage());
@@ -120,17 +122,17 @@ public class customerController {
         }
 
         if (customerService.findByEmail(modifyForm.getEmail()).isPresent()) {
-            throw new UserIdDuplicatedException("중복된 이메일이 존재합니다.");
+            throw new EmailDuplicatedException("중복된 이메일이 존재합니다.");
         }
 
 
         try {
             customerService.modify(context, modifyForm);
         } catch(EmailDuplicatedException e) {
-            bindingResult.reject("SignUpEmailDuplicated", e.getMessage());
+            bindingResult.reject("EmailDuplicatedException", e.getMessage());
             return "customer/modify";
         } catch(UserIdDuplicatedException e) {
-            bindingResult.reject("SignUpUserIdDuplicated", e.getMessage());
+            bindingResult.reject("UserIdDuplicatedException", e.getMessage());
         }
 
         String modifyMsg = Util.url.encode("회원정보 수정이 완료되었습니다");
@@ -146,17 +148,28 @@ public class customerController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/modifyPassword")
-    public String modifyPassword(@AuthenticationPrincipal CustomerContext context, String password, String modifyPassword) {
+    public String modifyPassword(@AuthenticationPrincipal CustomerContext context, @Valid ModifyPasswordForm modifyPasswordForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer/modifyPassword";
+        }
+
         Customer loginedCustomer = customerService.findByUsername(context.getUsername()).orElseThrow(
                 () -> new DataNotFoundException("Customer Not Found")
         );
 
-        if(!passwordEncoder.matches(password, loginedCustomer.getPassword())) {
-            String msg = Util.url.encode("현재 비밀번호가 틀립니다.");
-            return "redirect:/member/modifyPassword?msg=%s".formatted(msg);
+        if(!passwordEncoder.matches(modifyPasswordForm.getPassword(), loginedCustomer.getPassword())) {
+            throw new NotMatchPresentPassword("현재 비밀번호가 틀립니다.");
         }
 
-        customerService.modifyPassword(loginedCustomer, modifyPassword);
+        try {
+            customerService.modifyPassword(loginedCustomer, modifyPasswordForm.getModifyPassword());
+        } catch (DataNotFoundException e) {
+            bindingResult.reject("EmailDuplicatedException", e.getMessage());
+            return "customer/modifyPassword";
+        } catch (NotMatchPresentPassword e) {
+            bindingResult.reject("NotMatchPresentPassword", e.getMessage());
+            return "customer/modifyPassword";
+        }
 
         return "redirect:/customer/profile";
     }
@@ -167,13 +180,45 @@ public class customerController {
     }
 
     @PostMapping("/findUsername")
-    public String findUsername(Model model, String email) {
-        Customer customer = customerService.findByEmail(email).orElseThrow(
+    public String findUsername(Model model, @Valid FindUsernameForm findUsernameForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer/findUsername";
+        }
+
+        Customer customer = customerService.findByEmail(findUsernameForm.getEmail()).orElseThrow(
                 () -> new DataNotFoundException("등록된 아이디가 존재하지 않습니다.")
         );
 
         model.addAttribute("customer", customer);
 
-        return "member/findUsername";
+        return "customer/findUsername";
+    }
+
+    @GetMapping("/findPassword")
+    public String showFindPassword(@ModelAttribute FindPasswordForm findPasswordForm) {
+        return "customer/findPassword";
+    }
+
+    @PostMapping("/findPassword")
+    public String findPassword(@Valid FindPasswordForm findPasswordForm, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "customer/findPassword";
+        }
+
+        Customer customer = customerService.findByUsernameAndEmail(findPasswordForm.getUsername(), findPasswordForm.getEmail()).orElseThrow(
+                () -> new DataNotFoundException("등록된 사용자가 없습니다.")
+        );
+
+        String newPassword = RandomStringUtils.randomAlphanumeric(10);;
+
+        try {
+            customerService.setNewPassword(customer, newPassword);
+        } catch (DataNotFoundException e) {
+            bindingResult.reject("등록된 사용자가 없습니다.", e.getMessage());
+        }
+
+        emailService.sendSimpleMessage(customer, newPassword);
+
+        return "redirect:/customer/login";
     }
 }
