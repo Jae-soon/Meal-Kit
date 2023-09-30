6c67e378051bbcba18a55190f1f406f81a081658
Jae-soon <rnjswotns12@naver.com>
2023-09-30 19:51:04 +0900

    feat : set layout

--- src/main/resources/static/js/customer/join.js
+++ src/main/resources/static/js/customer/join.js
@@ -44,6 +44,8 @@ function join() {
 
     if (pw != pwConfirm) {
         alert("비밀번호가 맞지 않습니다.")
+        pw = ""
+        pwConfirm = ""
     } else {
         document.getElementById("joinForm").submit();
         alert("회원가입이 완료되었습니다.")
